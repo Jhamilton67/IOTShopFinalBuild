ccca1af08a392f10f94041fa9433c04994691c70
Jhamilton67 <71016013+Jhamilton67@users.noreply.github.com>
2022-04-20 21:29:03 +0100

    Revert "Delete IotShopFinalUnitTest directory"
    This reverts commit 16aae4af4cc25af1a93144d27df968e62195ccba.

---
+++ IotShopFinalUnitTest/StockTake/StockTakeUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using Bunit;
+using IOTShopDataAccess.Data;
+using IOTShopDataAccess.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IotShopFinalUnitTest.StockTake
+{
+    public class StockTakeUnitTest : TestContext
+    {
+        [Fact]
+        public void StockTakeTest1()
+        {
+            //var stocktakeMoq = new Mock<IStockTakeData>();
+            //stocktakeMoq.Setup(m => m.GetStockTakes())
+              //  .Returns(Task.FromResult(result: new StockTakeModel[] { new StockTakeModel() { StockID = 1224123, StockTakeCount = 4444, StockType = "Beer", StockTypePrice = 5000, StockTypeSize = "Smallpack", StockTypeQuantity = "six", StockTypeColour = "red" } }));
+
+            var messagingMoq = new Mock<IStockTakeData>();
+        }
+    }
+}
